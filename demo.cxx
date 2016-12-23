65a31bcc12d71befbddf65bfb68cead8f9a97af1
Florian Rhiem <f.rhiem@fz-juelich.de>
2016-12-23 10:41:58 +0100

    Improved CoordinateSystemRenderer

--- demo.cxx
+++ demo.cxx
@@ -5,6 +5,7 @@
 
 #include "VFRendering/View.hxx"
 #include "VFRendering/ArrowRenderer.hxx"
+#include "VFRendering/CoordinateSystemRenderer.hxx"
 #include "VFRendering/BoundingBoxRenderer.hxx"
 #include "VFRendering/CombinedRenderer.hxx"
 #include "VFRendering/IsosurfaceRenderer.hxx"
@@ -136,12 +137,16 @@ int main(void) {
     yzplane_renderer_ptr->setOption<VFRendering::IsosurfaceRenderer::Option::ISOVALUE>(0.0);
     auto arrow_renderer_ptr = std::make_shared<VFRendering::ArrowRenderer>(view);
     auto bounding_box_renderer_ptr = std::make_shared<VFRendering::BoundingBoxRenderer>(VFRendering::BoundingBoxRenderer::forCuboid(view, (geometry.min()+geometry.max())*0.5f, geometry.max()-geometry.min()));
+    auto coordinate_system_renderer_ptr = std::make_shared<VFRendering::CoordinateSystemRenderer>(view);
+    coordinate_system_renderer_ptr->setOption<VFRendering::CoordinateSystemRenderer::Option::AXIS_LENGTH>({0, 20, 20});
+    coordinate_system_renderer_ptr->setOption<VFRendering::CoordinateSystemRenderer::Option::NORMALIZE>(false);
 
     std::vector<std::shared_ptr<VFRendering::RendererBase>> renderers = {
         isosurface_renderer_ptr,
         yzplane_renderer_ptr,
         arrow_renderer_ptr,
-        bounding_box_renderer_ptr
+        bounding_box_renderer_ptr,
+        coordinate_system_renderer_ptr
     };
     view.renderers({{std::make_shared<VFRendering::CombinedRenderer>(view, renderers), {{0, 0, 1, 1}}}});
     view.setOption<VFRendering::View::Option::IS_VISIBLE_IMPLEMENTATION>("bool is_visible(vec3 position, vec3 direction) { return position.x >= 0; }");
